389611dac150e0e20bf1234b577211847145fa33
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2021-12-20 22:41:41 +0100

    [cryptography-demo] fixed styling

--- index.php
+++ index.php
@@ -18,47 +18,32 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Credit of debit kaartbetaling</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap" rel="stylesheet">
+    <title>CSE Demo</title>
     <link rel="stylesheet" type="text/css" href="css/cryptography-demo.css">
     <script>
         var keyUrl = 'public-keys.php';
         var keyPairs = '<?php echo json_encode($publicEncryptionKeys); ?>';
     </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<main class="rounded-box">
-    <header>
+<div class="container pt-5 rounded-box">
 
-    </header>
-    <aside>
-        <h2>Bestelgegevens</h2>
-        <dl>
-            <dt>Bedrag</dt>
-            <dd>€ 0,01</dd>
-            <dt>Begunstigde</dt>
-            <dd>Classic Carparts</dd>
-            <dt>Verloopt op</dt>
-            <dd>19-02-2021 15:23</dd>
-        </dl>
-    </aside>
-
+    <h1>CSE demo</h1>
     <form action="api/process.php" method="post" data-pay-encrypt-form>
         <fieldset>
-            <legend>Betaalgegevens</legend>
             <label for="card-holder">Kaarthouder</label>
-            <input id="card-holder" type="text" placeholder="Naam van de kaarthouder" name="cardholder" value="" required data-pay-encrypt-field />
+            <input id="card-holder" type="text" class="form-control" placeholder="Naam van de kaarthouder" name="cardholder" value="" required data-pay-encrypt-field />
         </fieldset>
 
-        <fieldset>
+        <fieldset class="mt-3">
             <div class="row-pan-cvc-group">
-                <div class="column-pan">
+                <div class="column-pan col-8">
                     <div class="form-element-container has-icon-container">
                         <label for="cardnumber">Kaartnummer</label>
-                        <div class="has-icon-wrap">
+                        <div class="has-icon-wrap form-group">
                             <span class="input-container">
-                                <input id="cardnumber" type="text" name="cardnumber" placeholder="Het nummer van uw credit- of debitkaart" value="" required data-pay-encrypt-field />
+                                <input id="cardnumber" class="form-control" type="text" name="cardnumber" placeholder="Het nummer van uw credit- of debitkaart" value="" required data-pay-encrypt-field />
                             </span>
 
                             <div class="icon-container">
@@ -68,21 +53,21 @@ try {
                     </div>
                 </div>
 
-                <div class="column-cvc">
-                    <div class="form-element-container">
+                <div class="column-cvc col-4 ps-3">
+                    <div class="form-element-container form-group">
                         <label for="cvc" data-cvc-label>CVC</label>
                         <span class="input-container">
-                            <input id="cvc" type="text" name="cardcvc" placeholder="123" required value=""  data-pay-encrypt-field />
+                            <input id="cvc" type="text" name="cardcvc" class="form-control" placeholder="123" required value=""  data-pay-encrypt-field />
                         </span>
                     </div>
                 </div>
             </div>
 
             <div class="row-expiry-group">
-                <div class="column-month">
-                    <div class="form-element-container">
+                <div class="column-month col-6 pe-2">
+                    <div class="form-element-container form-group">
                         <label for="month">Geldig tot maand</label>
-                        <select name="valid_thru_month" id="month" data-pay-encrypt-field>
+                        <select name="valid_thru_month" class="form-control" id="month" data-pay-encrypt-field>
                             <option value="" disabled selected>Kies</option>
                             <option value="01">01 - Januari</option>
                             <option value="02">02 - Februari</option>
@@ -99,10 +84,10 @@ try {
                         </select>
                     </div>
                 </div>
-                <div class="column-year">
-                    <div class="form-element-container">
+                <div class="column-year col-6 ps-2">
+                    <div class="form-element-container form-group">
                         <label for="year">Jaar</label>
-                        <select name="valid_thru_year" id="year"  data-pay-encrypt-field>
+                        <select name="valid_thru_year" id="year" class="form-control" data-pay-encrypt-field>
                             <option value="" disabled selected>Kies</option>
                             <option value="2021">2021</option>
                             <option value="2022">2022</option>
@@ -117,7 +102,7 @@ try {
             </div>
 
             <div class="form-element-container">
-                <button type="submit" data-loading-state="0" disabled="disabled">
+                <button type="submit" class="btn btn-primary" data-loading-state="0" disabled="disabled">
                     <span class="state-not-loading">Doorgaan</span>
                     <span class="state-loading">
                         <img src="img/spinner.gif" width="30" height="30" alt="Loading" />
@@ -126,7 +111,7 @@ try {
             </div>
         </fieldset>
     </form>
-</main>
+</div>
 
 <script type="module">
     import MyForm from './js/merchant.js';
